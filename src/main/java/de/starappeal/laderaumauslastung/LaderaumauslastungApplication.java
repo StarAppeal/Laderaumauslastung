32f82006c8ceba0e78f5a53141d7d5a84ad58156
StarAppeal <silkfan11@gmail.com>
2022-08-30 01:09:11 +0200

    remove unnecessary imports

--- src/main/java/de/starappeal/laderaumauslastung/LaderaumauslastungApplication.java
+++ src/main/java/de/starappeal/laderaumauslastung/LaderaumauslastungApplication.java
@@ -1,7 +1,5 @@
 package de.starappeal.laderaumauslastung;
 
-import de.starappeal.laderaumauslastung.service.FahrzeugService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
