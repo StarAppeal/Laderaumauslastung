c9770350ecf5207e5b06ad79b2cddd83378794e2
StarAppeal <silkfan11@gmail.com>
2022-08-30 01:22:27 +0200

    adds prototype of the other rest endpoints

--- src/main/java/de/starappeal/laderaumauslastung/controller/VehicleController.java
+++ src/main/java/de/starappeal/laderaumauslastung/controller/VehicleController.java
@@ -1,10 +1,10 @@
 package de.starappeal.laderaumauslastung.controller;
 
+import de.starappeal.laderaumauslastung.db.entity.Vehicle;
 import de.starappeal.laderaumauslastung.model.VehicleModel;
 import de.starappeal.laderaumauslastung.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,7 +21,41 @@ public class VehicleController {
 
     @GetMapping("/vehicles")
     public List<VehicleModel> findAll() {
-        return service.findAll().stream().map(VehicleModel::new).collect(Collectors.toList());
+        return service
+                .findAll()
+                .stream()
+                .map(VehicleModel::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/vehicles/{id}")
+    public VehicleModel findById(@PathVariable Long id) {
+        return new VehicleModel(service.findById(id));
+    }
+
+    @PostMapping("/vehicles")
+    public VehicleModel create(@RequestBody Vehicle vehicle) {
+        return new VehicleModel(service.create(vehicle));
+    }
+
+    @PostMapping("/vehicles/bulkCreate")
+    public List<VehicleModel> bulkCreate(@RequestBody List<Vehicle> vehicles) {
+        return service
+                .bulkCreate(vehicles)
+                .stream()
+                .map(VehicleModel::new)
+                .collect(Collectors.toList());
+    }
+
+    @PutMapping("/vehicles")
+    public VehicleModel update(@RequestBody Vehicle vehicle) {
+        return new VehicleModel(service.update(vehicle));
+    }
+
+    @DeleteMapping("/vehicles/{id}")
+    public String deleteById(@PathVariable Long id) {
+        service.delete(id);
+        return "OK"; //TODO: change this return type
     }
 
 }
