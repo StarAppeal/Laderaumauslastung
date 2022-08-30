69c7a470e19b14828f380e410c37257e4b5ad185
StarAppeal <silkfan11@gmail.com>
2022-08-30 18:52:29 +0200

    add ControllerAdvices for errorHandling

---
+++ src/main/java/de/starappeal/laderaumauslastung/controller/advice/VehicleNotFoundAdvice.java
@@ -0,0 +1,19 @@
+package de.starappeal.laderaumauslastung.controller.advice;
+
+import de.starappeal.laderaumauslastung.exception.VehicleNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class VehicleNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(VehicleNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String vehicleNotFoundHandler(VehicleNotFoundException exception) {
+        return exception.getMessage();
+    }
+}
